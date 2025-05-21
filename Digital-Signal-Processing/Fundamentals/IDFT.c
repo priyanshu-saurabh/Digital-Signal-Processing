2313ca586eb8198c501f1ecb29743b6ae6138c4d
Priyanshu Saurabh <128209878+priyanshu-saurabh@users.noreply.github.com>
2025-05-21 22:26:52 +0530

    Update IDFT.c

--- Digital-Signal-Processing/Fundamentals/IDFT.c
+++ Digital-Signal-Processing/Fundamentals/IDFT.c
@@ -1,13 +1,10 @@
-//
-// Created by Priyan on 17-04-2025.
-//
+//Inverse discrete fourier transformation
 #include <stdio.h>
 #include <math.h>
 
 #define PI 3.14159265358979323846
 
-// IDFT function: Computes real (x[]) and imaginary (y[]) parts of time-domain signal
-// from real-valued frequency-domain signal X[] (assuming X[] has only real part)
+
 void idft(double x[], double y[], double X[], int N) {
     double real, imag;
 
@@ -15,7 +12,7 @@ void idft(double x[], double y[], double X[], int N) {
         real = 0.0;
         imag = 0.0;
 
-        // Compute IDFT
+        
         for (int i = 0; i < N; i++) {
             double angle = 2 * PI * i * k / N;
             real += X[i] * cos(angle);   // Real part
@@ -35,10 +32,10 @@ int main() {
     printf("Enter the length of input signal (N): ");
     scanf("%d", &N);
 
-    // hold input (X[]) and output time-domain signal (x[], y[])
+    
     double x[N], y[N], X[N];
 
-    // frequency-domain input (assuming only real part)
+ 
     for (int i = 0; i < N; i++) {
         printf("Enter X[%d] (Real part only): ", i);
         scanf("%lf", &X[i]);
